24dcc2b5060264c401ecbc130fb09d7cac40597a
Malinde Godwin <malindegodwin9@gmail.com>
2025-06-18 09:41:40 +0300

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import MovieCard from "./Components/MovieCard";
+// import MovieCard from "./Components/MovieCard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <MovieCard
-        movie={{ title: "Tim's Film ", release_date: "2024" }}
-      ></MovieCard>
+      <Home />
     </>
   );
 }
