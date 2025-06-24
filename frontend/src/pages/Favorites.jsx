a8ceea98514596dac6d15ecf9a6df79890c94613
Malinde Godwin <malindegodwin9@gmail.com>
2025-06-24 17:35:06 +0300

    commit

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -5,25 +5,32 @@ import MovieCard from "../Components/MovieCard";
 function Favorites() {
   const { favorites } = useMovieContext();
 
-  if (favorites) {
+  if (favorites.lenght === 0) {
     return (
-      <div className="favorites">
-        <h2>Your Favorites</h2>
-        <div className="movies-grid">
-          {favorites.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
-        </div>
+      <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites and they will appear here!</p>
       </div>
     );
   }
 
   return (
-    <div className="favorites-empty">
-      <h2>No Favorite Movies Yet</h2>
-      <p>Start adding movies to your favorites and they will appear here!</p>
+    <div className="favorites">
+      <h2>Your Favorites</h2>
+      <div className="movies-grid">
+        {favorites.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
     </div>
   );
+
+  // return (
+  //   <div className="favorites-empty">
+  //     <h2>No Favorite Movies Yet</h2>
+  //     <p>Start adding movies to your favorites and they will appear here!</p>
+  //   </div>
+  // );
 }
 
 export default Favorites;
